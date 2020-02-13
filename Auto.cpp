2974ec666c5cc104b89062a862e02caf46bcd4e8
sivakumar0 <61009652+sivakumar0@users.noreply.github.com>
2020-02-13 17:01:42 +0530

    Add files via upload

---
+++ Auto.cpp
@@ -0,0 +1,25 @@
+/*
+ * Auto.cpp
+ *
+ *  Created on: 13-Feb-2020
+ *      Author: user
+ */
+
+#include <iostream>
+
+#include "Auto.h"
+
+using namespace std;
+
+
+
+//void C11feauture:: Modify()
+//{
+	/*C11feauture object;
+
+	object.var1 = 10;
+	object.var1 = 10.0;
+	object.var1 = 12.2200;
+
+	std::cout<<"function executed"<<std::endl;*/
+//}
